40dedb70842d4290d684cba1c4aae656011f88ad
brismuth <brismuth@gmail.com>
2014-04-09 23:10:32 -0600

    Adding initial project, based on demo.

---
+++ server/foodstorageserver.js
@@ -0,0 +1,16 @@
+Players = new Meteor.Collection("players");
+
+// On server startup, create some players if the database is empty.
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Players.find().count() === 0) {
+      var names = ["Grace Hopper",
+                   "Marie Curie",
+                   "Carl Friedrich Gauss",
+                   "Nikola Tesla",
+                   "Claude Shannon"];
+      for (var i = 0; i < names.length; i++)
+        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
+    }
+  });
+}
